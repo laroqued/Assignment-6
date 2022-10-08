f4c2956f8b3c5a2ea2fe412553d10144e678991a
Donnyves Laroque <40510674+laroqued@users.noreply.github.com>
2022-10-08 16:52:37 -0700

    Completed Larger or Smaller?

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,16 @@
+/* 
+Create an application that accepts two integers within two separate prompts. Then, display only the larger of the two within the browser window. Don’t forget to handle the fact that the two could be equal.
+ */
+
+prompt1 = window.prompt("Enter your first number");
+num1 = parseInt(prompt1, 10);
+prompt2 = window.prompt("Enter your second number");
+num2 = parseInt(prompt2, 10);
+
+if (num1 > num2) {
+  window.document.writeln(num1 + " is larger than " + num2);
+} else if (num2 > num1) {
+  window.document.writeln(num2 + " is larger than " + num1);
+} else if (num2 === num1) {
+  window.document.writeln(num1 + " and " + num2 + " are equal.");
+}
