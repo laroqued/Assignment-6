e71ba908ecfb4d42ea5ba468fd0362413fd065c7
Donnyves Laroque <40510674+laroqued@users.noreply.github.com>
2022-10-08 19:48:53 -0700

    Marco! Polo!  completed

---
+++ js/07-marco-polo.js
@@ -0,0 +1,9 @@
+for (var i = 1; i < 101; i++) {
+  if (i % 15 == 0) {
+    console.log(i + ": Marco! Polo!");
+  } else if (i % 3 == 0) {
+    console.log(i + ": Marco!");
+  } else if (i % 5 == 0) {
+    console.log(i + ": Polo");
+  }
+}
