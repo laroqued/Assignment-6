3362c906696e36643d6fff54f2c71fad089f8344
Donnyves Laroque <40510674+laroqued@users.noreply.github.com>
2022-10-08 18:23:45 -0700

    Completed The “Coin Flip” Game

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -2,6 +2,11 @@
 Create an application that accepts two integers within two separate prompts. Then, display only the larger of the two within the browser window. Don’t forget to handle the fact that the two could be equal.
  */
 
+var prompt1;
+var prompt2
+var num1;
+var num2;
+
 prompt1 = window.prompt("Enter your first number");
 num1 = parseInt(prompt1, 10);
 prompt2 = window.prompt("Enter your second number");
